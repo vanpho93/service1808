20f74d25f00afcede4c7808487cee46612f993ec
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-23 19:52:22 +0700

    init2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  ip: string;
+  constructor(private http: Http) {
+    this.http.get('http://ip.jsontest.com/')
+    .toPromise()
+    .then(res => res.json())
+    .then(resJSON => this.ip = resJSON.ip);
+  }
 }
