b8e851cb0ce34a0b29176de2d53556e67c31d0df
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-25 21:15:35 +0700

    remove

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { WordService } from './word.service';
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
@@ -9,17 +10,26 @@ import { WordService } from './word.service';
 
 export class ListComponent implements OnInit {
   words: Word[] = [];
-  constructor(private wordService: WordService) {
+
+  constructor(private wordService: WordService) {}
+
+  ngOnInit() {
     this.wordService.getWords()
     .then(words => this.words = words);
   }
 
-  ngOnInit() {
+  remove(id) {
+    this.wordService.removeWord(id)
+    .then(removedWord => {
+      const index = this.words.findIndex(word => word._id === removedWord._id);
+      this.words.splice(index, 1);
+    })
+    .catch(err => console.log(err.message));
   }
-
 }
 
 interface Word {
   en: string;
   vn: string;
+  _id: string;
 }
--- src/app/list/word.service.ts
+++ src/app/list/word.service.ts
@@ -12,9 +12,17 @@ export class WordService {
             .toPromise()
             .then(res => res.json());
     }
+
+    removeWord(id: string): Promise<Word> {
+        const url = 'http://localhost:3000/word/';
+        return this.http.delete(url + id)
+        .toPromise()
+        .then(res => res.json());
+    }
 }
 
 interface Word {
     en: string;
     vn: string;
+    _id: string;
 }
